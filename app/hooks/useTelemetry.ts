4e330f1fa392c19cb8380e83e7242465b230c0ee
avilash-b <avilash.bharti@gmail.com>
2025-07-26 12:42:44 +0530

    telemetry optimization

--- app/hooks/useTelemetry.ts
+++ app/hooks/useTelemetry.ts
@@ -1,7 +1,5 @@
 import { useEffect } from 'react';
 
-const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
-const TELEMETRY_ENDPOINT = `${baseUrl}/anonymous/source`; // You'll need to create this endpoint
 const TELEMETRY_SENT_KEY = 'source_key';
 
 export const useTelemetry = () => {
@@ -11,24 +9,37 @@ export const useTelemetry = () => {
       if (localStorage.getItem(TELEMETRY_SENT_KEY)) {
         return;
       }
+      // Mark telemetry as sent
+      localStorage.setItem(TELEMETRY_SENT_KEY, 'true');
 
       // Get the source query parameter
       const urlParams = new URLSearchParams(window.location.search);
-      const source = urlParams.get('src');
+      const sourcevalue = urlParams.get('src');
+      const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
+      let source : number = 0;
 
-      if (source) {
+      switch(sourcevalue){
+        case "li" : source = 1;
+        break;
+        case "in" : source = 2;
+        break;
+        case "re" : source = 3;
+        break;
+        default: source = -1
+      }
+
+      if (source !== -1) {
         try {
+
           // Send telemetry data
-          await fetch(TELEMETRY_ENDPOINT, {
+          await fetch(`${baseUrl}/anonymous/source`, {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
             },
             body: JSON.stringify({ source }),
           });
-
-          // Mark telemetry as sent
-          localStorage.setItem(TELEMETRY_SENT_KEY, 'true');
+          
         } catch (error) {
           console.error('Failed to send telemetry:', error);
         }
